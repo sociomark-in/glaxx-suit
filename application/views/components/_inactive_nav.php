251d78dddc18741adb28329a1261bdd30d54d77b
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-12 13:09:11 +0530

    Empty Repository

--- application/views/components/_inactive_nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="javascript:void(0)">
-            <img src="<?= base_url('assets/media/brand_assets/') ?>glaxx_icon.svg" alt="" height="30">
-            GLAXX
-        </a>
-    </div>
-</nav>
